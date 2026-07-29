cfe1574936c99d8be509e90bfcd5010836b78c98
Nicole Huh <nicole.huh@syncarpha.com>
2026-07-29 17:40:02 -0400

    env changed

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || '9sjpnub8',
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET || 'production',
   useCdn: true, // Uses Sanity's global edge network for ultra-fast response times
   apiVersion: '2024-01-01',
 });
